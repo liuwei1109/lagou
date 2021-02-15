23cce6a644603bbabdceb6c44f752bd20cea6704
来日可期 <15201145015>
2021-02-15 16:26:24 +0800

    3-3

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ module.exports = {
     linkActiveClass: 'active',
     // 自定义路由表规则
     extendRoutes (routes, resolve) {
+      console.log('----')
+      console.log(routes)
+      console.log('----')
       // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
       routes.splice(0)
 
